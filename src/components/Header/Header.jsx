cfe1cff20581f9df7b41720da5cee0f71c7fe615
danimkim <danimkim.dev@gmail.com>
2024-11-18 02:44:53 +0900

    feat: display text dynamically in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,18 @@
+import { genRandomInt, reactDescriptions } from "@utils";
 import "./Header.css";
 import Img from "@assets/react-core-concepts.png";
 
 export default function Header() {
+  const randomDescription =
+    reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
+
   return (
     <header>
       <img src={Img} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {randomDescription} React concepts you will need for almost any app you
+        are going to build!
       </p>
     </header>
   );
---
+++ src/utils/index.js
@@ -0,0 +1,5 @@
+export const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+export function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
