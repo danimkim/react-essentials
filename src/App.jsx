11770b87a89f5e2196974050d977e0275860e591
danimkim <danimkim.dev@gmail.com>
2024-11-18 03:26:05 +0900

    feat: add tab menus

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import Header from "@components/Header/Header";
-import { CORE_CONCEPTS } from "./constants";
-import CoreConcept from "./components/CoreConcept/CoreConcept";
+import Header from "@components/Header";
+import { CORE_CONCEPTS, TAB_MENUS } from "./constants";
+import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
 
 function App() {
+  const handleSelect = (selectedMenu) => {
+    console.log(`Selected Menu: ${selectedMenu}`);
+  };
+
   return (
     <div>
       <Header />
@@ -14,6 +19,16 @@ function App() {
               <CoreConcept key={concept.title} {...concept} />
             ))}
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            {TAB_MENUS.map((menu) => (
+              <TabButton key={menu} onSelect={() => handleSelect(menu)}>
+                {menu}
+              </TabButton>
+            ))}
+          </menu>
         </section>
       </main>
     </div>
