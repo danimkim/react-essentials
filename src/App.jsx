db7b87c6f07319276ce5fbc7f4a05c2c0c56fb7e
danimkim <danimkim.dev@gmail.com>
2024-11-18 03:02:41 +0900

    feat: add core concept section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import Header from "@components/Header/Header";
+import { CORE_CONCEPTS } from "./constants";
+import CoreConcept from "./components/CoreConcept/CoreConcept";
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((concept) => (
+              <CoreConcept key={concept.title} {...concept} />
+            ))}
+          </ul>
+        </section>
       </main>
     </div>
   );
